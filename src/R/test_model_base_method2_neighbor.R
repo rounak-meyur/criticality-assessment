75c2d97a8a2a5bc87020b52376c1387f664fa8c9
Shivani Garg <shivanigarg@Shivanis-MacBook-Pro-2.local>
2020-06-24 16:53:25 -0400

    latest neighbors

--- src/R/test_model_base_method2_neighbor.R
+++ src/R/test_model_base_method2_neighbor.R
@@ -6,7 +6,6 @@ library(spatstat.utils)
 library(matrixStats)
 library(graphics)
 library(doParallel)
-library(doMC)
 
 #---------------------------------------------------------------------------------------------------------------------------------------------------
 get.data = function(bus, gen, branch) {
@@ -146,8 +145,7 @@ base_case = function(bus, gen, branch) {
   bus_ind = which(S_data$bus$bus_id %in% nodes)
   C = as.matrix(gen.bus.con(S_data$bus, S_data$gen))[bus_ind,]
   gen_ind = which(colSums(C) == 1)
-  # C = C[,gen_ind]
-  
+
   d_max = S_data$dmax[bus_ind]
   d_min = S_data$dmin[bus_ind]
   g_max = S_data$gmax[gen_ind]
@@ -156,18 +154,18 @@ base_case = function(bus, gen, branch) {
   mat = as.matrix(as_edgelist(graph))
   mat = cbind(mat, 0)
   for (i in 1:nrow(mat)) {
-    index = which(branchdat[which(branchdat$fbus == mat[i, 1]), 2] == mat[i, 2])[1]
+    index = which(branch[which(branch$fbus == mat[i, 1]), 2] == mat[i, 2])[1]
     if (is.na(index)) {
-      index = which(branchdat[which(branchdat$fbus == mat[i, 2]), 2] == mat[i, 1])[1]
-      mat[i, 3] = which(branchdat$fbus == mat[i, 2])[index]
+      index = which(branch[which(branch$fbus == mat[i, 2]), 2] == mat[i, 1])[1]
+      mat[i, 3] = which(branch$fbus == mat[i, 2])[index]
     } else {
-      mat[i, 3] = which(branchdat$fbus == mat[i, 1])[index]
+      mat[i, 3] = which(branch$fbus == mat[i, 1])[index]
     }
   }
   for (i in 1:(nrow(mat) - 1)) {
     if (nrow(mat) > 1) {
       if (as.numeric(mat[i, 3]) == as.numeric(mat[i + 1,3])) {
-        mat[i + 1,3] = as.numeric(mat[i, 3]) + 1
+        mat[i + 1, 3] = as.numeric(mat[i, 3]) + 1
       }
     }
   }
@@ -178,12 +176,12 @@ base_case = function(bus, gen, branch) {
   return(list("C" = C, "S.comp" = S.comp, "flim" = flim, "edge_ind" = edge_ind, "bus_ind" = bus_ind, "gen_ind" = gen_ind))
 }
 
-calculate.crit = function(edge_ind, flim, f) {
+calculate.crit = function(edge_ind, flim, f, mul) {
   fail = 0
   success = 0
   crit = 0
   for (l in 1:length(edge_ind)) {
-    range = c(0, flim[l] * 1.5)
+    range = c(0, flim[l] * mul)
     if (inside.range(abs(f[l]), range)) {
       success = success + 1
     }
@@ -198,152 +196,244 @@ calculate.crit = function(edge_ind, flim, f) {
   return(crit)
 }
 
-neighbor_check = function(branch, mul) {
-  check.criticality = read.delim2("~/check-criticality.txt", header = FALSE, stringsAsFactors = FALSE)
+neighbor_check = function(branch, mul, st) {
+  check.criticality = read.delim2(st, header = FALSE, stringsAsFactors = FALSE)
+  line_id = c()
   for (line in 1:nrow(check.criticality)) {
     rem = check.criticality[line, 2]
     rem = strsplit(rem, ",")
     rem = rem[[1]]
-    for (l in 1:length(rem)) {
-      branch[as.integer(rem[l]), 6] = branch[as.integer(rem[l]), 6] * mul
-    }
+    line_id = append(line_id, rem)
+  }
+  line_id = unique(line_id)
+  for (l in 1:length(line_id)) {
+    branch[as.integer(line_id[l]), 6] = branch[as.integer(line_id[l]), 6] * mul
   }
   return(branch)
 }
 
-#---------------------------------------------------------------------------------------------------------------------------------------------------
-branchdat = read.csv("~/branchdat.csv")
-gendat = read.csv("~/gendat.csv")
-busdat = read.csv("~/busdat.csv")
+load_check = function(bus, mul, st) {
+  check.criticality = read.delim2(st, header = FALSE, stringsAsFactors = FALSE)
+  line_id = c()
+  for (line in 1:nrow(check.criticality)) {
+    rem = check.criticality[line, 2]
+    rem = strsplit(rem, ",")
+    rem = rem[[1]]
+    bus_id = append(bus_id, rem)
+  }
+  bus_id = unique(bus_id)
+  for (l in 1:length(bus_id)) {
+    bus[as.integer(bus_id[l]), 3] = bus[as.integer(bus_id[l]), 3] * mul
+  }
+  return(bus)
+}
 
-branchdat = neighbor_check(branchdat, 5)
-
-base_case_list = base_case(busdat, gendat, branchdat)
-
-iterations = 1000
-
-criticality = rep(0.0, nrow(branchdat) + 1)
-data = get.data(busdat, gendat, branchdat)
-
-require("doParallel")
-require("foreach")
-require("doMC")
-
-cl = makeCluster(28)
-registerDoParallel(cl)
-registerDoMC(28)
-
-ptime = system.time({
-c = foreach (line_removal = 1:nrow(branchdat), .packages = c("igraph", "Matrix", "pracma", "spatstat.utils"), .combine = rbind)  %dopar% {
-  svMisc::progress(line_removal, nrow(branchdat))
+full_model = function(rate, rateA_all, neighbor, neighbor_file, load, load_file) {
+  criticality_final = zeros(4, 3207)
   
-  branchdat = read.csv("~/branchdat.csv")
-  gendat = read.csv("~/gendat.csv")
-  busdat = read.csv("~/busdat.csv")
-  
-  branchdat[line_removal, 11] = 0
-  S_data = calculate.s(busdat, branchdat, gendat, data)
-  gs = S_data$graphs
-  total_nodes = 0
-  
-  for (graph in gs) {
-    nodes = as.numeric(as_ids(V(graph)))
-    total_nodes = total_nodes + length(nodes)
-    if (length(nodes) > 1) {
-      bus_ind = which(S_data$bus$bus_id %in% nodes)
-      C = as.matrix(gen.bus.con(S_data$bus, S_data$gen))[bus_ind,]
-      gen_ind = which(colSums(C) == 1)
-      if (!isempty(gen_ind)) {
-        C = C[,gen_ind]
+  for (v in rate) {
+
+    branchdat = read.csv("~/branchdat.csv")
+    gendat = read.csv("~/gendat.csv")
+    busdat = read.csv("~/busdat.csv")
+    
+    if (neighbor) {
+      branchdat = neighbor_check(branchdat, v, neighbor_file)
+    }
+    
+    if (load) {
+      busdat = load_check(busdat, v, load_file)
+    }
+    
+    base_case_list = base_case(busdat, gendat, branchdat)
+    
+    iterations = 1000
+    
+    data = get.data(busdat, gendat, branchdat)
+    
+    require("doParallel")
+    require("foreach")
+    
+    cl = makeCluster(28)
+    registerDoParallel(cl)
+    
+    ptime = system.time({
+      c = foreach (line_removal = 1:nrow(branchdat), .packages = c("igraph", "Matrix", "pracma", "spatstat.utils"), .combine = 'c')  %dopar% {
+
+        branchdat = read.csv("~/branchdat.csv")
+        gendat = read.csv("~/gendat.csv")
+        busdat = read.csv("~/busdat.csv")
         
-        d_max = S_data$dmax[bus_ind]
-        d_min = S_data$dmin[bus_ind]
-        g_max = S_data$gmax[gen_ind]
-        g_min = S_data$gmin[gen_ind]
-        
-        mat = as.matrix(as_edgelist(graph))
-        mat = cbind(mat, 0)
-        for (i in 1:nrow(mat)) {
-          index = which(branchdat[which(branchdat$fbus == mat[i, 1]), 2] == mat[i, 2])[1]
-          if (is.na(index)) {
-            index = which(branchdat[which(branchdat$fbus == mat[i, 2]), 2] == mat[i, 1])[1]
-            mat[i, 3] = which(branchdat$fbus == mat[i, 2])[index]
-          } else {
-            mat[i, 3] = which(branchdat$fbus == mat[i, 1])[index]
-          }
+        if (neighbor) {
+          branchdat = neighbor_check(branchdat, v, neighbor_file)
         }
-        for (i in 1:(nrow(mat) - 1)) {
-          if (nrow(mat) > 1) {
-            if (as.numeric(mat[i, 3]) == as.numeric(mat[i + 1, 3])) {
-              mat[i + 1,3] = as.numeric(mat[i, 3]) + 1
+        
+        if (load) {
+          busdat = load_check(busdat, v, load_file)
+        }
+        
+        criticality = 0
+        branchdat[line_removal, 11] = 0
+        S_data = calculate.s(busdat, branchdat, gendat, data)
+        gs = S_data$graphs
+        total_nodes = 0
+        
+        for (graph in gs) {
+          nodes = as.numeric(as_ids(V(graph)))
+          total_nodes = total_nodes + length(nodes)
+          if (length(nodes) > 1) {
+            bus_ind = which(S_data$bus$bus_id %in% nodes)
+            C = as.matrix(gen.bus.con(S_data$bus, S_data$gen))[bus_ind,]
+            gen_ind = which(colSums(C) == 1)
+            if (!isempty(gen_ind)) {
+              C = C[,gen_ind]
+              
+              d_max = S_data$dmax[bus_ind]
+              d_min = S_data$dmin[bus_ind]
+              g_max = S_data$gmax[gen_ind]
+              g_min = S_data$gmin[gen_ind]
+              
+              mat = as.matrix(as_edgelist(graph))
+              mat = cbind(mat, 0)
+              for (i in 1:nrow(mat)) {
+                index = which(branchdat[which(branchdat$fbus == mat[i, 1]), 2] == mat[i, 2])[1]
+                if (is.na(index)) {
+                  index = which(branchdat[which(branchdat$fbus == mat[i, 2]), 2] == mat[i, 1])[1]
+                  mat[i, 3] = which(branchdat$fbus == mat[i, 2])[index]
+                } else {
+                  mat[i, 3] = which(branchdat$fbus == mat[i, 1])[index]
+                }
+              }
+              for (i in 1:(nrow(mat) - 1)) {
+                if (nrow(mat) > 1) {
+                  if (as.numeric(mat[i, 3]) == as.numeric(mat[i + 1, 3])) {
+                    mat[i + 1, 3] = as.numeric(mat[i, 3]) + 1
+                  }
+                }
+              }
+              
+              edge_ind = as.numeric(as.vector(mat[,3]))
+              S.comp = S_data$S[edge_ind, bus_ind]
+              flim = S_data$flim[edge_ind]
+              
+              crit = 0
+              bad = 0
+              iter = 1
+              
+              while (iter <= iterations) {
+
+                C_base = base_case_list$C
+                S.comp_base = base_case_list$S.comp
+                flim_base = base_case_list$flim
+                edge_ind_base = base_case_list$edge_ind
+                bus_ind_base = base_case_list$bus_ind
+                gen_ind_base = base_case_list$gen_ind
+                
+                g_i = c()
+                for (l in 1:length(g_max)) {
+                  g_i = append(g_i, runif(1, g_min[l], g_max[l]))
+                }
+                Sg = sum(g_i)
+                
+                g_i_base = rep(0, length(gen_ind_base))
+                g_i_base[gen_ind] = g_i
+                
+                d_i = c()
+                while (isempty(d_i)) {
+                  d_i = calculate.di(d_max, Sg, g_max)
+                }
+                
+                d_i_base = rep(0, length(bus_ind_base))
+                d_i_base[bus_ind] = d_i
+                
+                p = (C %*% g_i) - d_i
+                p_base = (C_base %*% g_i_base) - d_i_base
+                
+                f = S.comp %*% p
+                f_base = S.comp_base %*% p_base
+                
+                base = calculate.crit(edge_ind_base, flim_base, f_base, rateA_all)
+                if (base == 1) {
+                  iter = iter + 1
+                  crit = crit + calculate.crit(edge_ind, flim, f, rateA_all)
+                }
+              }
+              criticality = criticality + (length(nodes) * crit / iterations)
             }
           }
         }
-        
-        edge_ind = as.numeric(as.vector(mat[,3]))
-        S.comp = S_data$S[edge_ind, bus_ind]
-        flim = S_data$flim[edge_ind]
-        
-        crit = 0
-        crit_base = 0
-        bad = 0
-        iter = 1
-        
-        while (iter <= iterations) {
-          svMisc::progress(iter, iterations)
-          
-          C_base = base_case_list$C
-          S.comp_base = base_case_list$S.comp
-          flim_base = base_case_list$flim
-          edge_ind_base = base_case_list$edge_ind
-          bus_ind_base = base_case_list$bus_ind
-          gen_ind_base = base_case_list$gen_ind
-          
-          g_i = c()
-          for (l in 1:length(g_max)) {
-            g_i = append(g_i, runif(1, g_min[l], g_max[l]))
-          }
-          Sg = sum(g_i)
-          
-          g_i_base = rep(0, length(gen_ind_base))
-          g_i_base[gen_ind] = g_i
-          
-          d_i = c()
-          while (isempty(d_i)) {
-            d_i = calculate.di(d_max, Sg, g_max)
-          }
-          
-          d_i_base = rep(0, length(bus_ind_base))
-          d_i_base[bus_ind] = d_i
-          
-          p = (C %*% g_i) - d_i
-          p_base = (C_base %*% g_i_base) - d_i_base
-          
-          f = S.comp %*% p
-          f_base = S.comp_base %*% p_base
-          
-          base = calculate.crit(edge_ind_base, flim_base, f_base)
-          if (base == 1) {
-            iter = iter + 1
-            crit_base = crit_base + 1
-            crit = crit + calculate.crit(edge_ind, flim, f)
-          }
-        }
-        criticality[line_removal + 1] = criticality[line_removal + 1] + (length(nodes) * crit / crit_base)
-      }
+        criticality = criticality / total_nodes
+        criticality = 1 - criticality
+        criticality
+      }})
+    
+    stopCluster(cl)
+    
+    for (i in 1:(nrow(branchdat))) {
+      criticality_final[which(rate == v), i + 1] = c[[i]]
     }
   }
-  criticality[line_removal + 1] = criticality[line_removal + 1] / total_nodes
-  criticality[line_removal + 1] = 1 - criticality[line_removal + 1]
-}})
-# ptime
-
-for (i in 1:(length(criticality)-1)) {
-  criticality[i + 1] = c[[i]]
+  
+  return(criticality_final)
 }
 
 #---------------------------------------------------------------------------------------------------------------------------------------------------
-write.csv(criticality, "criticality_base_1.5_2_neighbor5.csv")
-h = hist(criticality, breaks = 20, main = "Transmission Line Criticality Frequencies", xlab = "Criticality", ylab = "Count", col = "darkmagenta", 
-         freq = TRUE)
+criticality1.5N = full_model(c(3, 4, 5, 6), 1.5, TRUE, "~/check-criticality-1.5.txt", FALSE, "")
+criticality1.25N = full_model(c(3, 4, 5, 6), 1.25, TRUE, "~/check-criticality-1.25.txt", FALSE, "")
+criticality1.5L = full_model(c(0.9, 0.8, 0.7, 0.6), 1.5, FALSE, "", TRUE, "~/check-load-criticality-1.5.txt")
+criticality1.25L = full_model(c(0.9, 0.8, 0.7, 0.6), 1.25, FALSE, "", TRUE, "~/check-load-criticality-1.25.txt")
+
+write.csv(criticality1.5N, "criticality_base_1.5_2_neighbors.csv")
+write.csv(criticality1.25N, "criticality_base_1.25_2_neighbors.csv")
+write.csv(criticality1.5L, "criticality_base_1.5_2_loads.csv")
+write.csv(criticality1.25L, "criticality_base_1.25_2_loads.csv")
+
+#---------------------------------------------------------------------------------------------------------------------------------------------------
+h = hist(criticality1.5N[1,], breaks = 20, main = "Transmission Line Criticality Frequencies", xlab = "Criticality", ylab = "Count", 
+         col = "darkmagenta", freq = TRUE)
 text(h$mids, h$counts, labels = h$counts, adj = c(0.5, -0.5))
+
+#---------------------------------------------------------------------------------------------------------------------------------------------------
+criticality_base_1.5_2 = read.csv("~/criticality_base_1.5_2.csv")
+criticality_base_1.5_2_neighbors = read.csv("~/criticality_base_1.5_2_neighbors")
+e = read.delim2("~/check-criticality-1.5.txt", header = FALSE, stringsAsFactors = FALSE)
+x = e[,1]
+x = x + 1
+plot(criticality_base_1.5_2[x, 2], ylim = c(0, 1), col = "black")
+points(criticality_base_1.5_2_neighbors[x, 2], col = "red")
+points(criticality_base_1.5_2_neighbors[x, 3], col = "purple")
+points(criticality_base_1.5_2_neighbors[x, 4], col = "blue")
+points(criticality_base_1.5_2_neighbors[x, 5], col = "green")
+
+# criticality_base_1.25_2 = read.csv("~/criticality_base_1.25_2.csv")
+# criticality_base_1.25_2_neighbors = read.csv("~/criticality_base_1.25_2_neighbors")
+# e = read.delim2("~/check-criticality-1.25.txt", header = FALSE, stringsAsFactors = FALSE)
+# x = e[,1]
+# x = x + 1
+# plot(criticality_base_1.25_2[x, 2], ylim = c(0, 1), col = "black")
+# points(criticality_base_1.25_2_neighbors[x, 2], col = "red")
+# points(criticality_base_1.25_2_neighbors[x, 3], col = "purple")
+# points(criticality_base_1.25_2_neighbors[x, 4], col = "blue")
+# points(criticality_base_1.25_2_neighbors[x, 5], col = "green")
+
+# criticality_base_1.5_2 = read.csv("~/criticality_base_1.5_2.csv")
+# criticality_base_1.5_2_loads = read.csv("~/criticality_base_1.5_2_loads")
+# e = read.delim2("~/check-criticality-1.5.txt", header = FALSE, stringsAsFactors = FALSE)
+# x = e[,1]
+# x = x + 1
+# plot(criticality_base_1.5_2[x, 2], ylim = c(0, 1), col = "black")
+# points(criticality_base_1.5_2_loads[x, 2], col = "red")
+# points(criticality_base_1.5_2_loads[x, 3], col = "purple")
+# points(criticality_base_1.5_2_loads[x, 4], col = "blue")
+# points(criticality_base_1.5_2_loads[x, 5], col = "green")
+
+# criticality_base_1.25_2 = read.csv("~/criticality_base_1.5_2.csv")
+# criticality_base_1.25_2_loads = read.csv("~/criticality_base_1.5_2_loads")
+# e = read.delim2("~/check-criticality-1.25.txt", header = FALSE, stringsAsFactors = FALSE)
+# x = e[,1]
+# x = x + 1
+# plot(criticality_base_1.25_2[x, 2], ylim = c(0, 1), col = "black")
+# points(criticality_base_1.25_2_loads[x, 2], col = "red")
+# points(criticality_base_1.25_2_loads[x, 3], col = "purple")
+# points(criticality_base_1.25_2_loads[x, 4], col = "blue")
+# points(criticality_base_1.25_2_loads[x, 5], col = "green")
